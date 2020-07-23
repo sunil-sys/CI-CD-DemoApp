5c8f066aed3f96bccef444324aa2b9116d2942cc
unknown <sunil@kiratitsolutions.com>
2020-07-23 15:49:24 -0700

    initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+@EnableSwagger2
+@SpringBootApplication
+@EnableHystrix
+public class DemoApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.example.demo")).build();
+	   }
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+@RestController
+public class DemoController {
+	private static final Logger LOG = Logger.getLogger(DemoController.class.getName());
+	private static Map<String, Product> productRepo = new HashMap<>();
+	   static {
+	      Product honey = new Product();
+	      honey.setId("1");
+	      honey.setName("Honey");
+	      productRepo.put(honey.getId(), honey);
+	      
+	      Product almond = new Product();
+	      almond.setId("2");
+	      almond.setName("Almond");
+	      productRepo.put(almond.getId(), almond);
+	   }
+	   
+	   
+	   
+	   @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+	   public ResponseEntity<Object> delete(@PathVariable("id") String id) { 
+	      productRepo.remove(id);
+	      return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+	   }
+	   
+	   @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+	   public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) { 
+	      productRepo.remove(id);
+	      product.setId(id);
+	      productRepo.put(id, product);
+	      return new ResponseEntity<>("Product is updated successsfully", HttpStatus.OK);
+	   }
+	   
+	   @RequestMapping(value = "/products", method = RequestMethod.POST)
+	   public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+	      productRepo.put(product.getId(), product);
+	      return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+	   }
+	   
+	   @RequestMapping(value = "/products")
+	   @HystrixCommand(fallbackMethod = "fallback_sunil", commandProperties = {
+			   @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+			})
+	   private String fallback_sunil() {
+		   System.out.println("Fallback called");
+		   
+		   return "Request fails. It takes long time to response";
+		}
+	   
+	   
+	   public ResponseEntity<Object> getProduct() throws InterruptedException {
+		   System.out.println("get product called");
+		   LOG.log(Level.INFO, "get product called");
+		   
+		   //hold
+		   
+		   Thread.sleep(3000);
+	      return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+	   }
+}
